1faf6d53a0d8b4a49e38012e1dad71d6f9035aaa
Chun Cheng Lin <jinsenglin@users.noreply.github.com>
2015-10-08 15:28:02 +0800

    Update start-14.92.26.9.sh

--- start-140.92.26.9/start-14.92.26.9.sh
+++ start-140.92.26.9/start-14.92.26.9.sh
@@ -7,6 +7,7 @@ export NISE_IP_ADDRESS=140.92.26.9
 echo "NISE_IP_ADDRESS=$NISE_IP_ADDRESS"
 
 sudo route add -net 140.92.0.0 netmask 255.255.0.0 gw 140.92.26.1
+sudo route del -net 140.92.26.0 netmask 255.255.255.0 dev eth1
 
 ./scripts/start.sh
 
