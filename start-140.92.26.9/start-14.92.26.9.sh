0fe91a1c070cbcc2b84200d3e2672435ed11c3ce
Chun Cheng Lin <jinsenglin@users.noreply.github.com>
2015-10-08 15:30:54 +0800

    Update start-14.92.26.9.sh

--- start-140.92.26.9/start-14.92.26.9.sh
+++ start-140.92.26.9/start-14.92.26.9.sh
@@ -6,13 +6,10 @@ echo "NISE_IP_ADDRESS=$NISE_IP_ADDRESS"
 export NISE_IP_ADDRESS=140.92.26.9
 echo "NISE_IP_ADDRESS=$NISE_IP_ADDRESS"
 
+route -n
 sudo route add -net 140.92.0.0 netmask 255.255.0.0 gw 140.92.26.1
 sudo route del -net 140.92.26.0 netmask 255.255.255.0 dev eth1
+route -n
 
+cd /vagrant
 ./scripts/start.sh
-
-#route -n &&
-#sudo route add default gw 140.92.26.1 &&
-#route -n &&
-#sudo route del default gw 10.0.2.2 &&
-#route -n
