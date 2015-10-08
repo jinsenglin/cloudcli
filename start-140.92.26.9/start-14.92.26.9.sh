0fcae3b1d135e3beadff7c32fee56c26fa1ff3ae
Chun Cheng Lin <jinsenglin@users.noreply.github.com>
2015-10-08 14:09:36 +0800

    Update start-14.92.26.9.sh

--- start-140.92.26.9/start-14.92.26.9.sh
+++ start-140.92.26.9/start-14.92.26.9.sh
@@ -6,10 +6,10 @@ echo "NISE_IP_ADDRESS=$NISE_IP_ADDRESS"
 export NISE_IP_ADDRESS=140.92.26.9
 echo "NISE_IP_ADDRESS=$NISE_IP_ADDRESS"
 
-./scripts/start.sh &&
+./scripts/start.sh
 
-route -n &&
-sudo route add default gw 140.92.26.1 &&
-route -n &&
-sudo route del default gw 10.0.2.2 &&
-route -n
+#route -n &&
+#sudo route add default gw 140.92.26.1 &&
+#route -n &&
+#sudo route del default gw 10.0.2.2 &&
+#route -n
