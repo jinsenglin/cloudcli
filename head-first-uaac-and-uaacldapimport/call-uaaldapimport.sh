2302cd32d4588c4aa8e3944810eec568e4002137
jinseng <jinsenglin@iii.org.tw>
2015-12-22 12:27:20 +0800

    updated call-uaaldapimport.sh

--- head-first-uaac-and-uaacldapimport/call-uaaldapimport.sh
+++ head-first-uaac-and-uaacldapimport/call-uaaldapimport.sh
@@ -8,6 +8,23 @@ cf create-space -o org-jinsenglin space-jinsenglin
 
 # Reference: https://github.com/pivotalservices/uaaldapimport/blob/master/README.md
 
+<<DOTHISFIRST
+add a new client
+
+uaac client add -i
+Client name: bulkimport
+New client secret:
+Verify new client secret:
+scope (list): Press Enter
+authorized grant types (list): client_credentials
+authorities (list): cloud_controller.admin,scim.write
+access token validity (seconds): Press Enter
+refresh token validity (seconds): Press Enter
+redirect uri (list): Press Enter
+autoapprove (list): Press Enter
+signup redirect url (url): Press Enter
+DOTHISFIRST
+
 	export GOPATH=$HOME/golang-space
 	export PATH=$PATH:$GOPATH/bin
 
