6e459d177142044b5310fd7aad2eeea4a84306f6
jinseng <jinsenglin@iii.org.tw>
2016-01-14 14:22:31 +0800

    deleted init-iaas-for-devops

--- init-iaas-for-devops/create-ssh-key-pair.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-KEYPAIR_NAME=$1
-
-# create ssh key pair
-nova keypair-add $KEYPAIR_NAME > $KEYPAIR_NAME.pem
--- init-iaas-for-devops/link-router-and-subnet.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-ROUTER_ID=$1
-SUBNET_ID=$2
-
-neutron router-interface-add $ROUTER_ID $SUBNET_ID
